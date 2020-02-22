6e1dcac939121d01d61e451629bb6f563b60f839
yaozhili1997 <1144946637@qq.com>
2020-02-22 20:32:10 +0800

    刷题

---
+++ LeetCode/easy/src/test1.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @description: 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * @author: 姚志立
+ * @create: 2020-02-03 23:44
+ **/
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] ints = twoSum(nums, target);
+        System.out.println(ints);
+        String s = Arrays.toString(ints);
+        System.out.println(s);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    int[] ints = {i, j};
+                    return ints;
+                }
+            }
+        }
+        return null;
+    }
+
+}
+
+
